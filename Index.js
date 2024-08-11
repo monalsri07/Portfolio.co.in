7ff111625438f12048f3e9a6fb14087c233edfba
monalsri07 <monalsri07@gmail.com>
2024-08-11 15:53:47 +0530

    Add files via upload
    js file

---
+++ Index.js
@@ -0,0 +1,7 @@
+/*-----------------------------------Hamburger&nav------------------------------------------------------ */
+const hamburger=document.querySelector(".hamburger");
+const navMenu=document.querySelector(".navbar");
+hamburger.addEventListener("click",()=>{
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
